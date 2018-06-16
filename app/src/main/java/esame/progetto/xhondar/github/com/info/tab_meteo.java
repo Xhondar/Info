e4a875f177cb7ba927bf31dbac9eb07c9887733a
Adrian <potockiadrianmateusz@gmail.com>
2018-06-16 21:15:25 +0200

    Aggiunta immagini, modifica codice del meteo, con aggiunta di indice di codici delle condizioni climatiche

--- app/src/main/java/esame/progetto/xhondar/github/com/info/tab_meteo.java
+++ app/src/main/java/esame/progetto/xhondar/github/com/info/tab_meteo.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -34,6 +35,7 @@ import org.w3c.dom.Text;
 public class tab_meteo extends Fragment {
     tab t = new tab();
     String s = "";
+
     public void find_weather(String citta){
         String url = "http://api.openweathermap.org/data/2.5/weather?q=";
         if(citta == "Lubiana")
@@ -49,20 +51,104 @@ public class tab_meteo extends Fragment {
             public void onResponse(JSONObject response) {
                 try
                 {
+                    /*android:background="@drawable/logo"*/
+                    //TABELLA CODICI CON CORRISPONDENTE CONDIZIONE CLIMATICA
+
+                    /* THUNDERSTORMS / fulmini
+                    200	thunderstorm with light rain
+                    200	thunderstorm with light rain
+                    201	thunderstorm with rain
+                    202	thunderstorm with heavy rain
+                    210	light thunderstorm
+                    211	thunderstorm
+                    212	heavy thunderstorm
+                    221	ragged thunderstorm
+                    230	thunderstorm with light drizzle
+                    231	thunderstorm with drizzle
+                    232	thunderstorm with heavy drizzle
+                    */
+
+                    /* DRIZZLE / pioggerella
+                    300	light intensity drizzle
+                    301	drizzle
+                    302	heavy intensity drizzle
+                    310	light intensity drizzle rain
+                    311	drizzle rain
+                    312	heavy intensity drizzle rain
+                    313	shower rain and drizzle
+                    314	heavy shower rain and drizzle
+                    321	shower drizzle
+                    */
+
+                    /* RAIN / pioggia
+                    500	light rain
+                    501	moderate rain
+                    502	heavy intensity rain
+                    503	very heavy rain
+                    504	extreme rain
+                    511	freezing rain
+                    520	light intensity shower rain
+                    521	shower rain
+                    522	heavy intensity shower rain
+                    531	ragged shower rain
+                    */
+
+                    /* SNOW / neve
+                    600	light snow
+                    601	snow
+                    602	heavy snow
+                    611	sleet
+                    612	shower sleet
+                    615	light rain and snow
+                    616	rain and snow
+                    620	light shower snow
+                    621	shower snow
+                    622	heavy shower snow
+                     */
+
+                    /* ATMOSPHERE / atmosfera
+                    701	mist
+                    711	smoke
+                    721	haze
+                    731	sand, dust whirls
+                    741	fog
+                    751	sand
+                    761	dust
+                    762	volcanic ash
+                    771	squalls
+                    781	tornado
+                     */
+
+                    /* CLOUDS / nuvole
+                    800 clear sky
+                    801	few clouds
+                    802	scattered clouds
+                    803	broken clouds
+                    804	overcast clouds
+                     */
+
                     TextView temp, timeData;
                     temp = (TextView) getActivity().findViewById(R.id.tempMeteo);
                     timeData = (TextView) getActivity().findViewById(R.id.timeDate);
 
-                    //ImageView image = (ImageView) getActivity().findViewById(R.id.logo);
-                    int id = getContext().getResources().getIdentifier("logo.png", "drawable", getContext().getPackageName());
-                    //image.setId(id);
+                    ImageView image = (ImageView) getActivity().findViewById(R.id.weatherPicture);
+
+                    image.setImageResource(R.drawable.clouds);
+
                     JSONObject obj = response.getJSONObject("main");
                     JSONArray array = response.getJSONArray("weather");
-                    //JSONObject obj2 = array.getJSONObject(0);
+                    JSONObject obj2 = array.getJSONObject(0);
 
+                    String code = obj2.getString("id");
                     String temperature = String.valueOf(obj.getDouble("temp"));
-                    //String description = obj2.getString("description");
+                    String description = obj2.getString("description");
 
+                    if(Integer.parseInt(code) >= 200 || Integer.parseInt(code) <= 232)
+                    {
+
+                        /*LinearLayout l = (LinearLayout) getActivity().findViewById(R.id.constraintLayout);
+                        l.setBackgroundResource(R.drawable.berlino);*/
+                    }
                     //image.setImageResource(id);
 
                     temp.setText(temperature);
