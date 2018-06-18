667f4b451d12a54f16359dc751d2d3943bc2b65f
ArniKK1999 <kumaraku.arnold.itisvinci@gmail.com>
2018-06-18 14:00:17 +0200

    aggiunte le casistiche nella tab_mappa

--- app/src/main/java/esame/progetto/xhondar/github/com/info/tab_mappa.java
+++ app/src/main/java/esame/progetto/xhondar/github/com/info/tab_mappa.java
@@ -18,101 +18,58 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-        public class tab_mappa extends Fragment implements OnMapReadyCallback {
-            /*
-                    private GoogleMap googleMap;
-                    SupportMapFragment mSupportMapFragment;
+    public class tab_mappa extends Fragment implements OnMapReadyCallback {
+        GoogleMap map;
+        tab t = new tab();
+        String s = "";
+        SupportMapFragment mapFragment;
 
-                    @Override
-                    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-                    {
-                        super.onCreateView(inflater, container, savedInstanceState);
-                        View root = inflater.inflate(R.layout.tabmappa, null, false);
-                        mSupportMapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
-                        if (mSupportMapFragment == null) {
-                            FragmentManager fragmentManager = getFragmentManager();
-                            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-                            mSupportMapFragment = SupportMapFragment.newInstance();
-                            fragmentTransaction.replace(R.id.map, mSupportMapFragment).commit();
-                        }
-
-                        setUpMapIfNeeded();
-                        return root;
-                    }
-
-                    @Override
-                    public void onMapReady(final GoogleMap googleMap) {
-                        this.googleMap = googleMap;
-                        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                        this.googleMap.getUiSettings().setZoomControlsEnabled(true);
-                        this.googleMap.getUiSettings().setCompassEnabled(true);
-                        this.googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-                        this.googleMap.getUiSettings().setZoomGesturesEnabled(true);
-                        this.googleMap.getUiSettings().setRotateGesturesEnabled(true);
-
-                        //final LatLng pos1 = new LatLng(44.783878,10.879663);
-                        googleMap.addMarker(new MarkerOptions().position(new LatLng(22.7253, 75.8655)).title("Indore"));
-                        //googleMap.addMarker(new MarkerOptions().position(pos1));
-                        this.googleMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
-                            @Override
-                            public void onMapLoaded() {
-                                googleMap.animateCamera(CameraUpdateFactory.newLatLngBounds(LatLngBounds.builder().build(), 100));
-                            }
-                        });
-
-                    }
-
-                    @Override
-                    public void onPause() {
-                        super.onPause();
-                    }
-
-                    @Override
-                    public void onResume() {
-                        super.onResume();
-                        setUpMapIfNeeded();
-                    }
-
-                    private void setUpMapIfNeeded() {
-                        if (googleMap == null) {
-                            getMapAsync(this);
-                        }
-                    }
-                    */
-            GoogleMap map;
-
-            public static tab_mappa newInstance() {
-                tab_mappa fragment = new tab_mappa();
-                return fragment;
-            }
-
-            SupportMapFragment mapFragment;
-            @Override
-            public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-                View v = inflater.inflate(R.layout.tabmappa, container, false);
-                SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
-                mapFragment.getMapAsync(this);
-
-                return v;
-            }
-
-            @Override
-            public void onMapReady(GoogleMap googleMap){
-                map = googleMap;
-                this.map = googleMap;
-                this.map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-                this.map.getUiSettings().setZoomControlsEnabled(true);
-                this.map.getUiSettings().setCompassEnabled(true);
-                this.map.getUiSettings().setMyLocationButtonEnabled(true);
-                this.map.getUiSettings().setZoomGesturesEnabled(true);
-                this.map.getUiSettings().setRotateGesturesEnabled(true);
-
-                LatLng pp = new LatLng(44.783878,10.879663);
-                map.addMarker(new MarkerOptions().position(pp).title("Carpi"));
-                map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp, 8));
-            }
-
-
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+            View v = inflater.inflate(R.layout.tabmappa, container, false);
+            SupportMapFragment mapFragment = (SupportMapFragment) this.getChildFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+            return v;
         }
+
+        @Override
+        public void onMapReady(GoogleMap googleMap){
+            t = (tab) getActivity();
+            s = t.getS();
+
+            map = googleMap;
+            this.map = googleMap;
+            this.map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+            this.map.getUiSettings().setZoomControlsEnabled(true);
+            this.map.getUiSettings().setCompassEnabled(true);
+            this.map.getUiSettings().setMyLocationButtonEnabled(true);
+            this.map.getUiSettings().setZoomGesturesEnabled(true);
+            this.map.getUiSettings().setRotateGesturesEnabled(true);
+
+            LatLng pp_carpi = new LatLng(44.7823700, 10.8777000);
+            map.addMarker(new MarkerOptions().position(pp_carpi).title("Carpi"));
+            LatLng pp_berlino = new LatLng(52.520008, 13.404954);
+            map.addMarker(new MarkerOptions().position(pp_berlino).title("Berlino"));
+            LatLng pp_trieste = new LatLng(45.6433,13.7903);
+            map.addMarker(new MarkerOptions().position(pp_trieste).title("Trieste"));
+            LatLng pp_lubiana = new LatLng(46.056946, 14.505751);
+            map.addMarker(new MarkerOptions().position(pp_lubiana).title("Lubiana"));
+            LatLng pp_norimberga = new LatLng(49.460983, 11.061859);
+            map.addMarker(new MarkerOptions().position(pp_norimberga).title("Norimberga"));
+
+            switch (s){
+                case "Carpi":
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp_carpi, 9)); break;
+                case "Berlino":
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp_berlino, 9)); break;
+                case "Trieste":
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp_trieste, 9)); break;
+                case "Lubiana":
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp_lubiana, 9)); break;
+                case "Norimberga":
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(pp_norimberga, 9)); break;
+                default: break;
+            }
+        }
+    }
